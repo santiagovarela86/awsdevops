4defbbe9e07c60eff313c426957814976f7a4ace
Santiago Varela <daronz@hotmail.com>
2021-08-27 17:06:06 -0300

    trying with depends_on

--- tf_resources.tf
+++ tf_resources.tf
@@ -264,6 +264,8 @@ resource "aws_api_gateway_api_key" "awsdemo-apigateway-apikey" {
 
 resource "aws_api_gateway_deployment" "awsdemo-apigateway-deployment" {
   rest_api_id = aws_api_gateway_rest_api.awsdemo-apigateway-api.id
+
+  depends_on = [aws_api_gateway_integration.awsdemo-integration-getmessages]
 }
 
 resource "aws_api_gateway_stage" "awsdemo-apigateway-stage" {

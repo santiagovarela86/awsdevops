2ca6cc5c0a2c4fca64c5c0afaf6ca19220530a44
Santiago Varela <daronz@hotmail.com>
2021-08-27 21:02:04 -0300

    adding post endpoint

--- tf_resources.tf
+++ tf_resources.tf
@@ -258,17 +258,24 @@ resource "aws_api_gateway_resource" "awsdemo-message" {
   path_part   = "message"
 }
 
-resource "aws_api_gateway_resource" "awsdemo-message-date" {
-  rest_api_id = aws_api_gateway_rest_api.awsdemo-apigateway.id
-  parent_id   = aws_api_gateway_resource.awsdemo-message.id
-  path_part   = "{date}"
-}
-
 resource "aws_api_gateway_method" "awsdemo-getMessages" {
   rest_api_id   = aws_api_gateway_rest_api.awsdemo-apigateway.id
   resource_id   = aws_api_gateway_resource.awsdemo-message.id
   http_method   = "GET"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "awsdemo-postMessage" {
+  rest_api_id   = aws_api_gateway_rest_api.awsdemo-apigateway.id
+  resource_id   = aws_api_gateway_resource.awsdemo-message.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_resource" "awsdemo-message-date" {
+  rest_api_id = aws_api_gateway_rest_api.awsdemo-apigateway.id
+  parent_id   = aws_api_gateway_resource.awsdemo-message.id
+  path_part   = "{date}"
 }
 
 resource "aws_api_gateway_method" "awsdemo-getMessage" {
@@ -318,6 +325,20 @@ resource "aws_lambda_function" "awsdemo-getMessages" {
   }
 }
 
+resource "aws_lambda_function" "awsdemo-postMessage" {
+  filename         = "lambda_apigtw_to_dynamodb.zip"
+  function_name    = "awsdemo-postMessage"
+  handler          = "app.postMessage"
+  role             = aws_iam_role.iam-apigateway-serverless.arn
+  source_code_hash = filebase64sha256("lambda_apigtw_to_dynamodb.zip")
+  runtime          = "nodejs10.x"
+  description      = "Create new message item in DynamoDB table"
+
+  tags = {
+    Environment = "AWS-Demo"
+  }
+}
+
 resource "aws_lambda_function" "awsdemo-getMessage" {
   filename         = "lambda_apigtw_to_dynamodb.zip"
   function_name    = "awsdemo-getMessage"
@@ -340,6 +361,14 @@ resource "aws_lambda_permission" "awsdemo-getMessages" {
   source_arn    = "${aws_api_gateway_rest_api.awsdemo-apigateway.execution_arn}/*/*"
 }
 
+resource "aws_lambda_permission" "awsdemo-postMessage" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.awsdemo-postMessage.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.awsdemo-apigateway.execution_arn}/*/*"
+}
+
 resource "aws_lambda_permission" "awsdemo-getMessage" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -355,6 +384,15 @@ resource "aws_api_gateway_integration" "awsdemo-getMessages" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.awsdemo-getMessages.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "awsdemo-postMessage" {
+  rest_api_id             = aws_api_gateway_rest_api.awsdemo-apigateway.id
+  resource_id             = aws_api_gateway_method.awsdemo-postMessage.resource_id
+  http_method             = aws_api_gateway_method.awsdemo-postMessage.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.awsdemo-postMessage.invoke_arn
 }
 
 resource "aws_api_gateway_integration" "awsdemo-integration-getMessage" {
